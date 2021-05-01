707561eba167971da40c96fc72632f072aca2b4a
es <emil.kantaneva@tuni.fi>
2021-05-01 14:03:46 +0300

    added node

---
+++ site/kortit.js
@@ -0,0 +1,12 @@
+const data = require('./deck/deck.json');
+console.log(data);
+
+
+
+  function pullCard(){
+    let mycards = [];
+    mycards = getCards();
+
+
+    console.log("mycards: ", mycards);
+  }
